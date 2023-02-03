6ffac472559365393c356eee436d2ebd2c4c750e
AbdullahSattar <124356810+AbdullahSattar0@users.noreply.github.com>
2023-02-03 16:49:38 +0500

    assigment

---
+++ assigment-1/script.js
@@ -0,0 +1,19 @@
+// chapter 01
+// Question 01
+
+// alert("Assalamoalekum I Sir");
+
+//Question 02
+// alert("Error! please entera valid password");
+
+//Question 03
+// alert("Welcome to JS Land In Happy coding");
+
+// Question 84
+// alert("Welcome to JS Lond....");
+// alert("Happy Coding!");
+
+//Question 05
+
+var alert ("Hello!...I can run js through my web browser's console ");
+console.log(alert);
