1bab226a4319283bb0c335565a0de4d9c0968754
AbdullahSattar <124356810+AbdullahSattar0@users.noreply.github.com>
2023-02-11 22:52:42 +0500

    updated assigment 4

--- assigment  4/apps.js
+++ assigment  4/apps.js
@@ -302,3 +302,18 @@ document.write(greeting);
 
 
 //Q-7
+/*
+var time = prompt("Enter time in 24-hour clock format (e.g., 1900 for 7pm)");
+
+if (time >= 0000 && time < 1200) {
+  document.write("Good morning!");
+} else if (time >= 1200 && time < 1700) {
+  document.write("Good afternoon!");
+} else if (time >= 1700 && time < 2100) {
+  document.write("Good evening!");
+} else if (time >= 2100 && time <= 2359) {
+  document.write("Good night!");
+} else {
+  document.write("Invalid time entered.");
+}
+*/
